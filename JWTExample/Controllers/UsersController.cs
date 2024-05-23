96ccc3091b06fc570d5706d808d4dd6974c53ced
fuga002 <159753999+fuga002@users.noreply.github.com>
2024-05-23 21:03:01 +0500

    JWT example

---
+++ JWTExample/Controllers/UsersController.cs
@@ -0,0 +1,66 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace JWTExample.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UsersController : ControllerBase
+{
+
+    private readonly IHttpContextAccessor _contextAccessor;
+
+    public UsersController(IHttpContextAccessor contextAccessor)
+    {
+        _contextAccessor = contextAccessor;
+    }
+
+    [HttpGet]
+    [Authorize]
+    public async Task<IActionResult> Check()
+    {
+        return Ok("It is working");
+    }
+
+
+    [HttpPost("example")]
+    [Authorize]
+    public async Task<IActionResult> Calculate([FromQuery]int a, [FromQuery]  int b)
+    {
+        var c = a + b;
+        return Ok(c);
+    }
+
+    [HttpGet("token")]
+
+    public string GetToken()
+    {
+        var signinKey = System.Text.Encoding.UTF32.GetBytes("qwertyuiop1234567890qwertyuiop");
+        var claims = new List<Claim>()
+        {
+            new Claim(ClaimTypes.GivenName,"Maruf"),
+            new Claim(ClaimTypes.NameIdentifier,"qwert1234"),
+            new Claim(ClaimTypes.Role,"SuperAdmin")
+        };
+        var security = new JwtSecurityToken(issuer: "Blog.Api",audience: "Blog.Client",claims:claims,expires:DateTime.Now.AddMinutes(10),signingCredentials: new SigningCredentials( new SymmetricSecurityKey(signinKey),algorithm: "HS256") );
+
+        var token = new JwtSecurityTokenHandler().WriteToken(security);
+        return token;
+    }
+
+    [HttpGet("profile")]
+    
+    [Authorize]
+    public async Task<IActionResult> GetProfile()
+    {
+
+        var givenName = _contextAccessor.HttpContext!.User.FindFirstValue(claimType: ClaimTypes.GivenName);
+        var role = _contextAccessor.HttpContext!.User.FindFirstValue(claimType: ClaimTypes.Role);
+
+        return Ok(new {givenName, role});
+    }
+}
--- JWTExample/Program.cs
+++ JWTExample/Program.cs
@@ -1,11 +1,38 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddHttpContextAccessor();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).
+    AddJwtBearer(options =>
+    {
+        var signinKey = System.Text.Encoding.UTF32.GetBytes("qwertyuiop1234567890qwertyuiop");
+        options.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidIssuer = "Blog.Api",
+            ValidAudience = "Blog.Client",
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            IssuerSigningKey = new SymmetricSecurityKey(signinKey),
+            ValidateIssuerSigningKey = true,
+            ValidateLifetime = true,
+            ClockSkew = TimeSpan.Zero
+        };
+    });
+
+
+    
+
 
 var app = builder.Build();
 
@@ -16,8 +43,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
 
+app.UseHttpsRedirection();
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
